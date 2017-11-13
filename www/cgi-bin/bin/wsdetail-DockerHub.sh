8b3d0884cbc4d2b5d4902b4cb5ea755cf74f67c2
root <you@example.com>
2017-11-13 11:52:27 +0000

    Sub-components updated

--- www/cgi-bin/bin/wsdetail-DockerHub.sh
+++ www/cgi-bin/bin/wsdetail-DockerHub.sh
@@ -12,9 +12,14 @@ while read REPO SUBREPO USERNAME PASSWORD
 do
 	echo "DETAIL,DockerHub,${COUNT},${REPO},${SUBREPO},${USERNAME},${PASSWORD}"
 	COUNT=$((++COUNT))
+	J2USER=${USERNAME}
+	J2PASS=${PASSWORD}
+	write_global J2USER
+	write_global J2PASS
 done < ${SYSTEMPATH}/wsdetail_DockerHub
 if [[ ${COUNT} -eq 1 ]]
 then
 	echo "DETAIL,DockerHub,${COUNT},,,,"
 fi
 IFS=$THISIFS
+bash ${BIN}/dockerhub.sh
